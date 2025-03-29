09a90fa9576b66a676ab3d174ade6d2ebbf7a0ed
Levando10 <dolevan055@gmail.com>
2025-03-30 02:34:32 +0700

    update deploy cookie

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -55,6 +55,7 @@ async function userSignInController(req, res) {
       const tokenOption = {
         httpOnly: true,
         secure: true,
+        sameSite: "None",
       };
 
       res.cookie("token", token, tokenOption).status(200).json({
