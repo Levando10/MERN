d850311759a3fde225165d0c67ad103d7e38f655
SaNgoTin <phuoctungpbc02@gmail.com>
2025-03-31 00:25:14 +0700

    Test logging

--- backend/index.js
+++ backend/index.js
@@ -23,15 +23,15 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-
-app.use("/api", router);
-
 app.use((req, res, next) => {
   res.on("finish", function() {
     console.log(req.method, decodeURI(req.url), JSON.stringify(req.body), res.statusCode);
   });
   next();
 })
+
+app.use("/api", router);
+
 const PORT = process.env.PORT || 8080;
 
 connectDB()
