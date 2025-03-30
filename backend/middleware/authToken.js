6893b16db9aa7a1d985242b7ed331969883bfc97
SaNgoTin <phuoctungpbc02@gmail.com>
2025-03-30 23:43:11 +0700

    fix

--- backend/middleware/authToken.js
+++ backend/middleware/authToken.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken')
 
-async function authToken(req,res,next){
-    try{
+async function authToken(req, res, next) {
+    try {
         const token = req.cookies?.token
-        if(!token){
-            return res.status(200).json({
-                message : "Please Login...!",
-                error : true,
-                success : false
+        if (!token) {
+            return res.status(401).json({
+                message: "Please Login...!",
+                error: true,
+                success: false
             })
-        }
+        } 
 
-        jwt.verify(token, process.env.TOKEN_SECRET_KEY, function(err, decoded) {        
-            if(err){
+        jwt.verify(token, process.env.TOKEN_SECRET_KEY, function (err, decoded) {
+            if (err) {
                 console.log("error auth", err)
             }
 
@@ -22,12 +22,12 @@ async function authToken(req,res,next){
         });
 
 
-    }catch(err){
+    } catch (err) {
         res.status(400).json({
-            message : err.message || err,
-            data : [],
-            error : true,
-            success : false
+            message: err.message || err,
+            data: [],
+            error: true,
+            success: false
         })
     }
 }
