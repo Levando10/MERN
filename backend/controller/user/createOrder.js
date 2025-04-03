42d80c4c2abb39f669a8466498e3de7161054f38
Levando10 <dolevan055@gmail.com>
2025-04-04 02:36:39 +0700

    update fix deploy

--- backend/controller/user/createOrder.js
+++ backend/controller/user/createOrder.js
@@ -34,10 +34,10 @@ const createOrder = async (req, res) => {
         const orderPayment = {
           amount: totalAmount,
           orderCode: new Date().getTime(),
-          // returnUrl: `https://my-mern-shop.vercel.app/payment-success?userId=${userId}&address=${encodeURIComponent(address)}`,
-          // cancelUrl: `https://my-mern-shop.vercel.app/payment-failed?userId=${userId}&address=${encodeURIComponent(address)}`,
-          returnUrl: `http://localhost:3000/payment-success?userId=${userId}&address=${encodeURIComponent(address)}`,
-          cancelUrl: `http://localhost:3000/payment-failed?userId=${userId}&address=${encodeURIComponent(address)}`,
+          returnUrl: `https://my-mern-shop.vercel.app/payment-success?userId=${userId}&address=${encodeURIComponent(address)}`,
+          cancelUrl: `https://my-mern-shop.vercel.app/payment-failed?userId=${userId}&address=${encodeURIComponent(address)}`,
+          // returnUrl: `http://localhost:3000/payment-success?userId=${userId}&address=${encodeURIComponent(address)}`,
+          // cancelUrl: `http://localhost:3000/payment-failed?userId=${userId}&address=${encodeURIComponent(address)}`,
           description: "Pay shopping bills",
         };
 
